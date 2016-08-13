d39edc625f433c9f8ed41545850a234b728e9163
pavanonator <pp428@scarletmail.rutgers.edu>
2016-08-13 16:15:35 +0530

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,3 @@
-
 //////////////settings/////////
 var actionZ = 0; //on left click action
 var rotationA = 3.1; // amount of rotation
@@ -32,3 +31,75 @@ var particles = new THREE.ParticleSystem( geometry, material );
 scene.add( particles ); 
 
 camera.position.x = -10000;
+
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+container.appendChild( renderer.domElement );
+
+renderer.render( scene, camera );
+
+render();
+
+			function render() {
+        requestAnimationFrame( render );
+        if (!rotated && camera.position.x < 11000)
+        {
+          if(camera.position.x > 10000){
+            rotated = true;
+            if (camera.rotation.y < rotationA){
+              camera.rotation.y += .015;
+              rotated = false;
+            }
+            if (camera.position.z > -2000){
+              camera.position.z -= 19;
+              rotated = false;
+            }
+          }
+          else{
+          camera.position.x += movementSpeed;
+          camera.position.z += actionZ;
+          }
+        }
+        else if(rotated && camera.position.x > -11000){
+					if(camera.position.x < -10000){
+            rotated = false;
+            if (camera.rotation.y > 0){
+              camera.rotation.y -= .015;
+              rotated = true;
+            }
+            if (camera.position.z < 2000){
+              camera.position.z += 19;
+              rotated = true;
+            }
+          }
+          else{
+          camera.position.x -= movementSpeed;
+          camera.position.z -= actionZ;
+          }
+        }
+        
+           
+				renderer.render( scene, camera );
+
+			}
+
+window.addEventListener( 'mousedown', onDocumentMouseDown, false );
+window.addEventListener( 'mouseup', onDocumentMouseUp, false );
+window.addEventListener( 'resize', onWindowResize, false );
+
+function onWindowResize() {
+				camera.aspect = window.innerWidth / window.innerHeight;
+				camera.updateProjectionMatrix();
+
+				renderer.setSize( window.innerWidth, window.innerHeight );
+
+			}
+function onDocumentMouseDown(){
+    event.preventDefault();
+    actionZ = -zoomSpeed;
+}
+
+function onDocumentMouseUp(){
+   actionZ = 0;
+}

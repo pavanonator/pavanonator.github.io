5123f1317373cdec03ac57bf20315ab090001a33
pavanonator <pp428@scarletmail.rutgers.edu>
2016-08-13 16:05:43 +0530

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,12 @@
 
+//////////////settings/////////
+var actionZ = 0; //on left click action
+var rotationA = 3.1; // amount of rotation
+var movementSpeed = 10;
+var zoomSpeed = 10;
+var totalObjects = 40000;
+/////////////////////////////////
+var rotated = false; 
+var container = document.createElement('div');
+document.body.appendChild( container );
+

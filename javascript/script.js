3046caee29e9d85cdc0b1b4af6955bbeae40a482
pavanonator <pp428@scarletmail.rutgers.edu>
2016-08-13 16:06:15 +0530

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -10,3 +10,25 @@ var rotated = false;
 var container = document.createElement('div');
 document.body.appendChild( container );
 
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight,1, 10000)
+camera.position.z = 2000; 
+
+var scene = new THREE.Scene();
+scene.fog = new THREE.FogExp2( 0x555555, 0.0003 );  
+var geometry = new THREE.Geometry();
+
+for (i = 0; i < totalObjects; i ++) 
+{ 
+  var vertex = new THREE.Vector3();
+  vertex.x = Math.random()*40000-20000;
+  vertex.y = Math.random()*7000-3500;
+  vertex.z = Math.random()*7000-3500;
+  geometry.vertices.push( vertex );
+}
+
+var material = new THREE.ParticleBasicMaterial( { size: 6 });
+var particles = new THREE.ParticleSystem( geometry, material );
+	  
+scene.add( particles ); 
+
+camera.position.x = -10000;
